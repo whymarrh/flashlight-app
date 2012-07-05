9a77754e0f212f935a2abd386870fe8ae09c5aab
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2012-07-05 08:26:14 -0400

    Increased readability.

--- src/tld/www/flashlight/Flashlight.java
+++ src/tld/www/flashlight/Flashlight.java
@@ -8,37 +8,41 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class Flashlight extends Activity {
+
   private Window window = null;
-  private float brightness = 100.0f;
   private WindowManager.LayoutParams wmlp = null;
-  private PackageManager pm = null;
   private Camera camera = null;
   private Camera.Parameters camera_parameters = null;
 
-  @Override public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-
-    // set the screen brightness to 100%
+  private void setScreenBrightness(float brightness) {
     window = getWindow();
     wmlp = window.getAttributes();
     wmlp.screenBrightness = brightness / 100.0f;
     window.setAttributes(wmlp);
-
-    // activate the camera's flash
-    pm = getPackageManager();
-    boolean hasFlash = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-    if (hasFlash) {
-      camera = Camera.open();
-      camera_parameters = camera.getParameters();
-      camera_parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-      camera.setParameters(camera_parameters);
-      camera.startPreview();
-    }
   }
-  @Override public void onPause() {
-    super.onPause();
+  private void activateCameraTorch() {
+    boolean hasFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    camera = Camera.open();
+    if (camera == null || !hasFlash) return;
+    camera_parameters = camera.getParameters();
+    camera_parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+    camera.setParameters(camera_parameters);
+    camera.startPreview();
+  }
+  private void releaseCameraTorch() {
+    if (camera == null) return;
     camera.stopPreview();
     camera.release();
   }
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    activateCameraTorch();
+    setScreenBrightness(100.0f);
+  }
+  @Override public void onPause() {
+    super.onPause();
+    releaseCameraTorch();
+  }
+
 }
